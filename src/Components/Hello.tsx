a3f2077b1d59f10833c3760c4a3d20740b60f6cf
webInRoad <1040233545@qq.com>
2021-02-11 08:15:57 +0800

    hello world提交

---
+++ src/Components/Hello.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface IHelloProps {
+	message?:string
+}
+// const Hello = (props: IHelloProps) => {
+// 	return <div>{props.message}</div>
+// }
+// const hello:React.FunctionComponent<IHelloProps> = (props) => {
+// 	return <div>{props.message}</div>
+// }
+const hello:React.FC<IHelloProps> = (props) => {
+	return <div>{props.message}</div>
+}
+hello.defaultProps = {
+	message:'Hello World'
+}
+export default hello
