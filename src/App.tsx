69a35675605588fe9ba48116b5b68e5e3b1506bf
webInRoad <1040233545@qq.com>
2021-02-11 22:55:00 +0800

    HOC实现loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,26 @@ import Hello from './Components/Hello'
 import LinkButton from './Components/LinkButton'
 import Mouse from './Components/Mouse'
 import useMousePosition from './hooks/useMounsePosition'
+import withLoader from './Components/withLoader'
 import logo from './logo.svg';
 import './App.css';
 
+interface IShowResult {
+  message: string;
+  status: string;
+}
+const DogShow: React.FC<{data:IShowResult}> = ({data}) => {
+  return (
+    <>
+      <h2>Dog show: {data.status}</h2>
+      <img src={data.message} />
+    </>
+  )
+}
 function App() {
   const [show,setShow] = useState(false)
   const position = useMousePosition()
+  const WrappedDogShow = withLoader(DogShow,'https://dog.ceo/api/breeds/image/random')
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +32,8 @@ function App() {
         </button>
         <div>位于X:{position.x},y:{position.y}</div>
         {/* {show && <Mouse/>} */}
-        {<LinkButton />}
+        {/* {<LinkButton />} */}
+        <WrappedDogShow />
         <a
           className="App-link"
           href="https://reactjs.org"
