9e5447a25af01a2ecf6567eaf19763584fe1dee8
webInRoad <1040233545@qq.com>
2021-02-14 08:10:43 +0800

    代码优化

--- src/hooks/useMounsePosition.tsx
+++ src/hooks/useMounsePosition.tsx
@@ -1,16 +1,16 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 const useMousePosition = () => {
-    const [position,setPosition] = useState({x:0,y:0})
-    const updatePosition = (e:MouseEvent) => {
-        setPosition({x:e.clientX,y:e.clientY})
-    }
-    useEffect(() => {
-        document.addEventListener('mousemove',updatePosition)
-        return () => {
-            document.removeEventListener('mousemove',updatePosition)
-        }
-    })
-    return position
+	const [position, setPosition] = useState({ x: 0, y: 0 })
+	const updatePosition = (e: MouseEvent) => {
+		setPosition({ x: e.clientX, y: e.clientY })
+	}
+	useEffect(() => {
+		document.addEventListener('mousemove', updatePosition)
+		return () => {
+			document.removeEventListener('mousemove', updatePosition)
+		}
+	})
+	return position
 }
 export default useMousePosition
